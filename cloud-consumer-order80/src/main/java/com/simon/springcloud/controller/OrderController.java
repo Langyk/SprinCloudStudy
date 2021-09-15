60b129a13e038a9bba51ee8a0720079e8994386e
Simon <yakunlang@163.com>
2021-09-15 22:06:24 +0800

    '修改支付客户端'

--- cloud-consumer-order80/src/main/java/com/simon/springcloud/controller/OrderController.java
+++ cloud-consumer-order80/src/main/java/com/simon/springcloud/controller/OrderController.java
@@ -5,6 +5,7 @@ import com.simon.springcloud.entites.Payment;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
@@ -27,7 +28,7 @@ public class OrderController {
      * REST请求地址、请求参数、HTTP响应转换被转换成的对象类型。
      */
     @GetMapping("/consumer/payment/create") //客户端用浏览器是get请求，但是底层实质发送post调用服务端8001
-    public CommonResult create(Payment payment)
+    public CommonResult create(@RequestBody Payment payment)
     {
         return restTemplate.postForObject(PaymentSrv_URL + "/payment/create",payment,CommonResult.class);
     }
