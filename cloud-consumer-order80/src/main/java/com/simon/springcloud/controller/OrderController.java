3495f8fdd0404dd98fbc2e67357a0480d622127f
Simon <yakunlang@163.com>
2021-09-15 22:36:33 +0800

    '工程重构'

--- cloud-consumer-order80/src/main/java/com/simon/springcloud/controller/OrderController.java
+++ cloud-consumer-order80/src/main/java/com/simon/springcloud/controller/OrderController.java
@@ -1,7 +1,7 @@
 package com.simon.springcloud.controller;
 
-import com.simon.springcloud.entites.CommonResult;
-import com.simon.springcloud.entites.Payment;
+import com.simon.springcloud.entities.CommonResult;
+import com.simon.springcloud.entities.Payment;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
