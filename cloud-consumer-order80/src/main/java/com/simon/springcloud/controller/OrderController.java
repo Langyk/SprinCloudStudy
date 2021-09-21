e404ea41a594c1dc3b39373b26766c676a944e44
Simon <yakunlang@163.com>
2021-09-21 22:44:57 +0800

    '构建Eureka集群'

--- cloud-consumer-order80/src/main/java/com/simon/springcloud/controller/OrderController.java
+++ cloud-consumer-order80/src/main/java/com/simon/springcloud/controller/OrderController.java
@@ -18,7 +18,9 @@ import org.springframework.web.client.RestTemplate;
 @RestController
 public class OrderController {
 
-    public static final String PaymentSrv_URL = "http://localhost:8001";
+    //    public static final String PaymentSrv_URL = "http://localhost:8001";
+// 通过在eureka上注册过的微服务名称调用
+    public static final String PAYMENT_SRV = "http://CLOUD-PAYMENT-SERVICE";
 
     @Autowired
     private RestTemplate restTemplate;
@@ -28,15 +30,13 @@ public class OrderController {
      * REST请求地址、请求参数、HTTP响应转换被转换成的对象类型。
      */
     @GetMapping("/consumer/payment/create") //客户端用浏览器是get请求，但是底层实质发送post调用服务端8001
-    public CommonResult create(@RequestBody Payment payment)
-    {
-        return restTemplate.postForObject(PaymentSrv_URL + "/payment/create",payment,CommonResult.class);
+    public CommonResult create(@RequestBody Payment payment) {
+        return restTemplate.postForObject(PAYMENT_SRV + "/payment/create", payment, CommonResult.class);
     }
 
     @GetMapping("/consumer/payment/get/{id}")
-    public CommonResult getPayment(@PathVariable Long id)
-    {
-        return restTemplate.getForObject(PaymentSrv_URL + "/payment/get/"+id, CommonResult.class, id);
+    public CommonResult getPayment(@PathVariable Long id) {
+        return restTemplate.getForObject(PAYMENT_SRV + "/payment/get/" + id, CommonResult.class, id);
     }
 
 }
