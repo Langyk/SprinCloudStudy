9c4515a76e20b320a576b153e7e0607c07bc6427
Simon <yakunlang@163.com>
2021-09-25 19:15:35 +0800

    '服务提供者注册进zookerper'

---
+++ cloud-provider-payment8004/src/main/java/com/simon/springcloud/controller/PaymentController.java
@@ -0,0 +1,25 @@
+package com.simon.springcloud.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.UUID;
+
+/**
+ * @Author: Simon Lang
+ * @Description: Think Twice, Code Once
+ * @Date: 2021/9/25 18:39
+ */
+
+@RestController
+public class PaymentController {
+    @Value("${server.port}")
+    private String serverPort;
+
+    @RequestMapping(value = "/payment/zk")
+    public String paymentzk()
+    {
+        return "springcloud with zookeeper: "+serverPort+"\t"+ UUID.randomUUID().toString();
+    }
+}
