266d7f61e86be2eb68ae8f3dff6cae2555572258
Simon <yakunlang@163.com>
2021-09-25 21:05:46 +0800

    '消费者注入zookerper'

---
+++ cloud-consumerzk-order80/src/main/java/com/simon/springcloud/config/ApplicationContextBean.java
@@ -0,0 +1,23 @@
+package com.simon.springcloud.config;
+
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+/**
+ * @Author: Simon Lang
+ * @Description: Think Twice, Code Once
+ * @Date: 2021/9/25 19:42
+ */
+@Configuration
+public class ApplicationContextBean
+{
+    @Bean
+    @LoadBalanced
+    public RestTemplate getRestTemplate()
+    {
+        return new RestTemplate();
+    }
+}
+
