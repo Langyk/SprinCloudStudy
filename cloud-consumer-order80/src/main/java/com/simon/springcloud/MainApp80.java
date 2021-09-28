4d40c5260b3dfce4a745bfa96a486cc4b8424221
Simon <yakunlang@163.com>
2021-09-28 21:25:59 +0800

    '替换Ribbon中默认的负载均衡算法'

--- cloud-consumer-order80/src/main/java/com/simon/springcloud/MainApp80.java
+++ cloud-consumer-order80/src/main/java/com/simon/springcloud/MainApp80.java
@@ -1,8 +1,10 @@
 package com.simon.springcloud;
 
+import com.simon.myrule.MySelfRule;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
 
 /**
  * @Author: Simon Lang
@@ -12,6 +14,7 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableEurekaClient
+@RibbonClient(name = "CLOUD-PAYMENT-SERVICE",configuration = MySelfRule.class)
 public class MainApp80 {
     public static void main(String[] args) {
         SpringApplication.run(MainApp80.class,args);
