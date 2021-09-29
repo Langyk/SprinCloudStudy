d49765cfbe68c7119c7385982e714e9feb13c20f
Simon <yakunlang@163.com>
2021-09-29 20:46:57 +0800

    'openfeign的基本使用'

---
+++ cloud-consumer-feign-order80/src/main/java/com/simon/springcloud/controller/OrderFeignController.java
@@ -0,0 +1,27 @@
+package com.simon.springcloud.controller;
+
+import com.simon.springcloud.entities.CommonResult;
+import com.simon.springcloud.service.PaymentFeignService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+
+/**
+ * @Author: Simon Lang
+ * @Description: Think Twice, Code Once
+ * @Date: 2021/9/29 20:28
+ */
+@RestController
+public class OrderFeignController
+{
+    @Resource
+    private PaymentFeignService paymentFeignService;
+
+    @GetMapping(value = "/consumer/payment/get/{id}")
+    public CommonResult getPaymentById(@PathVariable("id") Long id)
+    {
+        return paymentFeignService.getPaymentById(id);
+    }
+}
