bfd11517e27d27369bf7e0bdc6bf48a78527dcc9
Simon <yakunlang@163.com>
2021-10-01 11:02:12 +0800

    '编写consumer-feign-hystrix消费者服务'

---
+++ cloud-consumer-feign-hystrix-order80/src/main/java/com/simon/springcloud/OrderHystrixMain80.java
@@ -0,0 +1,18 @@
+package com.simon.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+/**
+ * @Author: Simon Lang
+ * @Description: Think Twice, Code Once
+ * @Date: 2021/10/1 10:51
+ */
+@SpringBootApplication
+@EnableFeignClients
+public class OrderHystrixMain80 {
+    public static void main(String[] args) {
+        SpringApplication.run(OrderHystrixMain80.class,args);
+    }
+}
---
+++ cloud-consumer-feign-hystrix-order80/src/main/java/com/simon/springcloud/service/PaymentHystrixService.java
@@ -0,0 +1,21 @@
+package com.simon.springcloud.service;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+/**
+ * @Author: Simon Lang
+ * @Description: Think Twice, Code Once
+ * @Date: 2021/10/1 10:53
+ */
+@Component
+@FeignClient(value = "CLOUD-PROVIDER-HYSTRIX-PAYMENT")
+public interface PaymentHystrixService {
+    @GetMapping("/payment/hystrix/ok/{id}")
+    String paymentInfo_OK(@PathVariable("id") Integer id);
+
+    @GetMapping("/payment/hystrix/timeout/{id}")
+    String paymentInfo_TimeOut(@PathVariable("id") Integer id);
+}
