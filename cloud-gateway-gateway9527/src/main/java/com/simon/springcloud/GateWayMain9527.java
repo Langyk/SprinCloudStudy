4bf27b9f56bf86a3d27b101feb9c11ec304e46fa
Simon <yakunlang@163.com>
2021-10-02 23:33:37 +0800

    'Gatway网关的两种配置方式'

---
+++ cloud-gateway-gateway9527/src/main/java/com/simon/springcloud/GateWayMain9527.java
@@ -0,0 +1,19 @@
+package com.simon.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * @Author: Simon Lang
+ * @Description: Think Twice, Code Once
+ * @Date: 2021/10/2 15:38
+ */
+
+@SpringBootApplication
+@EnableEurekaClient
+public class GateWayMain9527 {
+    public static void main(String[] args) {
+        SpringApplication.run(GateWayMain9527.class,args);
+    }
+}
