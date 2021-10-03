88148a63cac265f4ce0441d4b30d02cef9727451
Simon <yakunlang@163.com>
2021-10-03 14:19:01 +0800

    'config客户端配置与测试'

---
+++ cloud-config-client-3355/src/main/java/com/simon/springcloud/controller/ConfigClientController.java
@@ -0,0 +1,23 @@
+package com.simon.springcloud.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Author: Simon Lang
+ * @Description: Think Twice, Code Once
+ * @Date: 2021/10/3 11:31
+ */
+
+@RestController
+public class ConfigClientController {
+    @Value("${config.info}")
+    private String configInfo;
+
+    @GetMapping("/configInfo")
+    public String getConfigInfo()
+    {
+        return configInfo;
+    }
+}
