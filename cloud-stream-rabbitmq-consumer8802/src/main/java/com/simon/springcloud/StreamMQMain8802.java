e294521a8c982933d2d16b08d96a2a247b0ddc24
Simon <yakunlang@163.com>
2021-10-04 09:54:16 +0800

    '分组消费'

---
+++ cloud-stream-rabbitmq-consumer8802/src/main/java/com/simon/springcloud/StreamMQMain8802.java
@@ -0,0 +1,17 @@
+package com.simon.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Author: Simon Lang
+ * @Description: Think Twice, Code Once
+ * @Date: 2021/10/4 8:52
+ */
+@SpringBootApplication
+public class StreamMQMain8802 {
+    public static void main(String[] args)
+    {
+        SpringApplication.run(StreamMQMain8802.class,args);
+    }
+}
