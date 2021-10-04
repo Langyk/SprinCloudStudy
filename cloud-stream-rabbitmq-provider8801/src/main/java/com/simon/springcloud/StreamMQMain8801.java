6ad70c4546f3cf7894f13c1df9dd0c585f3737ab
Simon <yakunlang@163.com>
2021-10-04 08:47:51 +0800

    '消息驱动之生产者'

---
+++ cloud-stream-rabbitmq-provider8801/src/main/java/com/simon/springcloud/StreamMQMain8801.java
@@ -0,0 +1,16 @@
+package com.simon.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Author: Simon Lang
+ * @Description: Think Twice, Code Once
+ * @Date: 2021/10/3 23:33
+ */
+@SpringBootApplication
+public class StreamMQMain8801 {
+    public static void main(String[] args) {
+        SpringApplication.run(StreamMQMain8801.class,args);
+    }
+}
---
+++ cloud-stream-rabbitmq-provider8801/src/main/java/com/simon/springcloud/service/IMessageProvider.java
@@ -0,0 +1,10 @@
+package com.simon.springcloud.service;
+
+/**
+ * @Author: Simon Lang
+ * @Description: Think Twice, Code Once
+ * @Date: 2021/10/3 23:49
+ */
+public interface IMessageProvider {
+    public String send() ;
+}
